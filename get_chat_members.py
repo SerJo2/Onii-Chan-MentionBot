e48cb5c69a33ebd91e76b3888dc5bdae0ec02096
SerJo2 <termenator1ser@gmail.com>
2025-05-31 20:25:39 +1000

    change print to logging

--- get_chat_members.py
+++ get_chat_members.py
@@ -1,17 +1,17 @@
 from telethon import TelegramClient
 from prefs import *
-print("telethon start")
+import logging
+
+logging.info("Telethon was started")
+
 client = TelegramClient('session_name', api_id, api_hash)
-print("telethon set up")
+logging.info("Telethon client was set up")
 async def get_chat_members(chat_id):
-    print("telethon func start")
+    logging.info("Telethon func start")
     await client.start(bot_token=token)
     chat_members = []
-    print("telethon before for")
     async for member in client.iter_participants(chat_id):
-        print("telethon iteration")
         chat_members.append(member.username)
-    print("telethon end cycle")
     await client.disconnect()
-    print("telethon disconnected")
+    logging.info("Telethon disconnected")
     return chat_members
--- main.py
+++ main.py
@@ -3,26 +3,25 @@ from prefs import *
 from get_chat_members import get_chat_members
 
 import asyncio
-import csv
+import logging
 
 from telebot.async_telebot import AsyncTeleBot
-print("ok")
+
+logging.info("Main.py started")
 API_TOKEN = token
 
-
 bot = AsyncTeleBot(API_TOKEN)
-print("bot set up")
+logging.info("Bot was set up")
 @bot.message_handler(content_types=['text'])
 async def get_text_messages(message):
     try:
-        print(message.text)
-        print(message.chat.id)
+        logging.info("message.text: " + message.text)
+        logging.info("message.chat.id: " + message.chat.id)
 
 
         if message.text == "/all":
-            print("before func")
             chat_members = await get_chat_members(message.chat.id)
-            print(chat_members)
+            logging.info("chat_members: " + chat_members)
             for i in range(1, len(chat_members), 5):
                 group = chat_members[i:i+5]
                 send = ""
@@ -40,6 +39,7 @@ async def get_text_messages(message):
                 pass
     except BaseException as be:
         await bot.send_message(message.chat.id, "напишите @psibladeabuzerz \n" + "Что-то пошло не так. Лог ниже \n" % be)
+        logging.critical("Ошибка в get_text_messages", exc_info=True)
 
 
 asyncio.run(bot.polling())

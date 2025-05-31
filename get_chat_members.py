845f4cefa8c8e2e8cc9ddbebecced58ea39dc058
SerJo2 <termenator1ser@gmail.com>
2025-05-31 20:15:20 +1000

    add try...except for unhandled errors. Add check for my chat id

--- get_chat_members.py
+++ get_chat_members.py
@@ -2,16 +2,16 @@ from telethon import TelegramClient
 from prefs import *
 print("telethon start")
 client = TelegramClient('session_name', api_id, api_hash)
-print("telethon setted up")
+print("telethon set up")
 async def get_chat_members(chat_id):
     print("telethon func start")
     await client.start(bot_token=token)
     chat_members = []
-    print("telerhon before for")
+    print("telethon before for")
     async for member in client.iter_participants(chat_id):
         print("telethon iteration")
         chat_members.append(member.username)
     print("telethon end cycle")
     await client.disconnect()
-    print("telethon disconected")
+    print("telethon disconnected")
     return chat_members
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
-from prefs import token
+from prefs import *
 from get_chat_members import get_chat_members
 
 import asyncio
+import csv
 
 from telebot.async_telebot import AsyncTeleBot
 print("ok")
@@ -13,22 +14,32 @@ bot = AsyncTeleBot(API_TOKEN)
 print("bot set up")
 @bot.message_handler(content_types=['text'])
 async def get_text_messages(message):
-    print(message.text)
-    if message.text == "/all":
-        print("before func")
-        chat_members = await get_chat_members(message.chat.id)
-        print(chat_members)
-        for i in range(1, len(chat_members), 5):
-            group = chat_members[i:i+5]
-            send = ""
-            for j in group:
-                k = "@" + j
-                send = send + k + " "
-            await bot.send_message(message.chat.id, send)
-    if message.text == "/ping":
-        await bot.send_message(message.chat.id, "Бот работает")
-    if message.text == "/help":
-        await bot.send_message(message.chat.id, "/all - Пинг всех в группе \n /ping - Проверка онлайна бота")
+    try:
+        print(message.text)
+        print(message.chat.id)
+
+
+        if message.text == "/all":
+            print("before func")
+            chat_members = await get_chat_members(message.chat.id)
+            print(chat_members)
+            for i in range(1, len(chat_members), 5):
+                group = chat_members[i:i+5]
+                send = ""
+                for j in group:
+                    k = "@" + j
+                    send = send + k + " "
+                await bot.send_message(message.chat.id, send)
+        if message.text == "/ping":
+            await bot.send_message(message.chat.id, "Бот работает")
+        if message.text == "/help":
+            await bot.send_message(message.chat.id, "/all - Пинг всех в группе \n /ping - Проверка онлайна бота")
+
+        if message.chat.id == my_chat_id:
+            if message.text == "":
+                pass
+    except BaseException as be:
+        await bot.send_message(message.chat.id, "напишите @psibladeabuzerz \n" + "Что-то пошло не так. Лог ниже \n" % be)
 
 
 asyncio.run(bot.polling())

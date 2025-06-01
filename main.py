b840810b0a7f34b1696f90c08f9f876292cdbf92
SerJo2 <termenator1ser@gmail.com>
2025-06-02 08:51:26 +1000

    fixed logger, add msg thread send

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pyexpat.errors import messages
 
 from prefs import *
 from get_chat_members import get_chat_members
@@ -18,24 +19,29 @@ baseLogger.info("Bot was set up")
 @bot.message_handler(content_types=['text'])
 async def get_text_messages(message):
     try:
-        baseLogger.info("message.text: " + message.text)
-        baseLogger.info("message.chat.id: " + message.chat.id)
+        baseLogger.info("message.text: " + str(message.text))
+        baseLogger.info("message.chat.id: " + str(message.chat.id))
+
+        try:
+            msg_thread_id = message.reply_to_message.message_thread_id
+        except AttributeError:
+            msg_thread_id = "General"
 
 
         if message.text == "/all":
             chat_members = await get_chat_members(message.chat.id)
-            baseLogger.info("chat_members: " + chat_members)
+            baseLogger.info("chat_members: " + str(chat_members))
             for i in range(1, len(chat_members), 5):
                 group = chat_members[i:i+5]
                 send = ""
                 for j in group:
                     k = "@" + j
                     send = send + k + " "
-                await bot.send_message(message.chat.id, send)
+                await bot.send_message(message.chat.id, send, message_thread_id=msg_thread_id)
         if message.text == "/ping":
-            await bot.send_message(message.chat.id, "Бот работает")
-        if message.text == "/help":
-            await bot.send_message(message.chat.id, "/all - Пинг всех в группе \n /ping - Проверка онлайна бота")
+            await bot.send_message(message.chat.id, "Бот работает", message_thread_id=msg_thread_id)
+        if message.text == "/ocHelp":
+            await bot.send_message(message.chat.id, "/all - Пинг всех в группе \n/ping - Проверка онлайна бота", message_thread_id=msg_thread_id)
 
         if message.chat.id == my_chat_id:
             if message.text == "":
